b0748774de10a803e45d27fab52cfc12711d32c3
ilhampratamahambali <122896583+ilhampratamahambali@users.noreply.github.com>
2025-02-09 02:05:41 +0700

    menambahkan icon

--- app/Views/pengguna/login_page.php
+++ app/Views/pengguna/login_page.php
@@ -6,6 +6,8 @@
     <title>Login Page</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <!-- Favicon -->
+        <link href="<?php echo base_url(''); ?>logo.png" rel="icon">
     <style>
         * {
             margin: 0;
